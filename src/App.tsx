cf58426ba4b2870356dc7edbceb1838681a9ab58
CAWilson94 <charlotteawilson.94@gmail.com>
2023-06-04 09:02:44 +0100

    basic routing between homepage and whale

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import Narwhal from './components/Narwhal/Narwhal';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Narwhal from "./components/Narwhal/Narwhal";
+import Whale from "./components/Whale/Whale";
 function App() {
   return (
     <div className="wrapper">
       <h1> Example For Router with Marine Animals</h1>
-      <Narwhal />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Narwhal/>}></Route>
+          <Route path="/whale" element={<Whale/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
